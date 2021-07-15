52dd479aab920924f03ffcad0892f6effce47632
“Danyal <danyalkhanzada89@gmail.com>
2021-07-15 17:13:10 -0400

    testing

--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,31 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReplySchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    replyBody: {
+      type: String
+    },
+    writtenBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
 
 const CommentSchema = new Schema(
   {
@@ -27,31 +52,9 @@ const CommentSchema = new Schema(
   }
 );
 
-const ReplySchema = new Schema(
-  {
-    // set custom id to avoid confusion with parent comment's _id field
-    replyId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId()
-    },
-    replyBody: {
-      type: String
-    },
-    writtenBy: {
-      type: String
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: createdAtVal => dateFormat(createdAtVal)
-    }
-  },
-  {
-    toJSON: {
-      getters: true
-    }
-  }
-);
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
+});
 
 const Comment = model('Comment', CommentSchema);
 
